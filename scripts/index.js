423750c417dff29458fd07177f2c2911a58c97d4
Ivan Chirkov <strangeevan@gmail.com>
2023-12-25 12:34:57 +0300

    Код на ревью

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,19 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard (placeItem) {
+  const placesList = document.querySelector('.places__list');
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = placeItem.link;
+  cardElement.querySelector('.card__title').textContent = placeItem.name;
+
+  placesList.append(cardElement); 
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', function() {cardElement.remove();});
+}
+
+initialCards.forEach((placeItem) => {createCard(placeItem);});
